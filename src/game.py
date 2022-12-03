4ee2a1102ff6b0cb964578ef06f885c1eeda0f13
Roni-kemp <lord.of.lamancha.r@gmail.com>
2022-12-03 20:27:31 +0200

    Update game.py

--- src/game.py
+++ src/game.py
@@ -1,6 +1,7 @@
 
 import pygame
 from .settings import Settings
+from .level_1 import Level
 
 class Game:
     def __init__(self, settings: Settings):
@@ -8,18 +9,22 @@ class Game:
         self.settings = settings
         self.clock = pygame.time.Clock()
         self.running = True
+        self.level = Level()
 
     def handle_events(self) -> None:
         events = pygame.event.get()
         for event in events:
             if event.type == pygame.QUIT:
                 self.running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    self.running = False
 
     def run(self) -> None:
         while self.running:
             self.handle_events()
             self.clock.tick(self.settings.frame_rate)
-
+            self.level.run()
             pygame.display.flip()
 
 
