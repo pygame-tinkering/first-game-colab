2386ec897d299bae9aba250af1247b8f89ef7c1e
Roni-kemp <lord.of.lamancha.r@gmail.com>
2022-12-05 18:48:51 +0200

    minimal background

--- src/level_1.py
+++ src/level_1.py
@@ -1,5 +1,4 @@
 import pygame 
-# from settings import *
 from .player import Player
 
 class Level:
@@ -7,7 +6,9 @@ class Level:
 		self.gameObject = game
 		# get the display surface
 		self.display_surface = pygame.display.get_surface()
-
+		self.background = pygame.image.load("./assets/images/background/bg.png").convert_alpha()
+		# print(self.gameObject.settings)
+		self.background= pygame.transform.scale(self.background, (self.gameObject.screen.get_width(), self.gameObject.screen.get_height()))
 		# sprite groups
 		self.all_sprites = pygame.sprite.Group()
 
@@ -18,5 +19,6 @@ class Level:
 
 	def run(self):
 		self.display_surface.fill('black')
+		self.display_surface.blit(self.background, (0, 0))
 		self.all_sprites.update()
 		self.all_sprites.draw(self.display_surface)
